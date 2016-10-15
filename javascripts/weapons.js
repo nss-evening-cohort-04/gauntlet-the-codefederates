8bc30e2ff76cce13a61947b0bb6b8af5f35a5a42
Taylor Shuff <tkylesh@gmail.com>
2016-10-15 12:02:56 -0500

    pulling from master again

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -1,13 +1,9 @@
 
 var Gauntlet =(function (gauntletWeapons) {
 
-<<<<<<< HEAD
-gauntletWeapons.Weapon = function() {
-=======
 gauntletWeapons.WeaponBarracks = {};
 
 gauntletWeapons.WeaponBarracks.Weapon = function() {
->>>>>>> 39d8a46a97b87ef10727fed35b8e4e0b5583a024
   this.weaponName = "bare hands";
   this.weaponDamage = 2;
 
@@ -16,21 +12,16 @@ gauntletWeapons.WeaponBarracks.Weapon = function() {
   }
 };
 
-<<<<<<< HEAD
-gauntletWeapons.talkingPoints = function() {
-=======
+
 gauntletWeapons.WeaponBarracks.talkingPoints = function() {
->>>>>>> 39d8a46a97b87ef10727fed35b8e4e0b5583a024
   this.weaponName = "talking Points";
   this.weaponDamage = 4;
 };
 gauntletWeapons.WeaponBarracks.talkingPoints.prototype = new gauntletWeapons.WeaponBarracks.Weapon();
 
-<<<<<<< HEAD
-gauntletWeapons.experience = function() {
-=======
+
+
 gauntletWeapons.WeaponBarracks.experience = function() {
->>>>>>> 39d8a46a97b87ef10727fed35b8e4e0b5583a024
   this.weaponName = "experience";
   this.weaponDamage = 5;
 };
