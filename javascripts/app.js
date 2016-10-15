e25f8a515ad1474501b9f51a2204b870670825d1
Taylor Shuff <tkylesh@gmail.com>
2016-10-15 11:00:47 -0500

    working on the battleground

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,24 +1,26 @@
 /*
   Test code to generate a human player and an orc player
  */
-var warrior = new Gauntlet.Combatants.Human();
-warrior.setWeapon(new WarAxe());
-warrior.generateClass();  // This will be used for "Surprise me" option
-console.log(warrior.toString());
+// var warrior = new Gauntlet.Combatants.Human();
+// warrior.setWeapon(new WarAxe());
+// warrior.generateClass();  // This will be used for "Surprise me" option
+// console.log(warrior.toString());
 
-var orc = new Gauntlet.Combatants.Orc();
-orc.generateClass();
-orc.setWeapon(new BroadSword());
-console.log(orc.toString());
+// var orc = new Gauntlet.Combatants.Orc();
+// orc.generateClass();
+// orc.setWeapon(new BroadSword());
+// console.log(orc.toString());
 
 /*
   Test code to generate a spell
  */
-var spell = new Gauntlet.SpellBook.Sphere();
-console.log("spell: ", spell.toString());
+// var spell = new Gauntlet.SpellBook.Sphere();
+// console.log("spell: ", spell.toString());
+
 
 
 $(document).ready(function() {
+  Gauntlet.attack();
   /*
     Show the initial view that accepts player name
    */
--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -1,25 +1,25 @@
-var Gauntlet =(function () {
+var Gauntlet =(function (gauntletWeapon) {
 
-var Weapon = function() {
-  this.weaponName = "bare hands";
-  this.weaponDamage = 2;
+	gauntletWeapon.Weapon = function() {
+	  this.weaponName = "bare hands";
+	  this.weaponDamage = 2;
 
-  this.toString = function() {
-    return this.name;
-  }
-};
+	  this.toString = function() {
+	    return this.name;
+	  }
+	};
 
-var talkingPoints = function() {
-  this.weaponName = "talking Points";
-  this.weaponDamage = 4;
-};
-talkingPoints.prototype = new Weapon();
+	gauntletWeapon.talkingPoints = function() {
+	  this.weaponName = "talking Points";
+	  this.weaponDamage = 4;
+	};
+	talkingPoints.prototype = new Weapon();
 
-var experience = function() {
-  this.weaponName = "experience";
-  this.weaponDamage = 5;
-};
-experience.prototype = new Weapon();
+	gauntletWapon.experience = function() {
+	  this.weaponName = "experience";
+	  this.weaponDamage = 5;
+	};
+	experience.prototype = new Weapon();
 
-
+	return gauntletWeapon;
 })(Gauntlet || {});
