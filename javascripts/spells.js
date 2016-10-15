269bc460c01d24700fd110b89b46f2977f746dae
Alicia Miceli <aliciamiceli@Alicias-MacBook-Pro.local>
2016-10-15 10:43:28 -0500

    two spells

--- javascripts/spells.js
+++ javascripts/spells.js
@@ -1,33 +1,31 @@
-/*
-  TODO: Modularize this code with IIFE or Browserify
- */
-var Gauntlet = Gauntlet || {};
-Gauntlet.SpellBook = {};
+var Gauntlet  = (function(){
 
+Gauntlet.SpellBook = {};
 
 /*
   Base spell function that defines name, damage, damage type
  */
 Gauntlet.SpellBook.Spell = function() {
-  this.name = "";
-  this.damage = 0;
-
-  this.damageTypes = ["lightning", "fire", "water", "earth", "mysticism"];
-  this.type = "";
+  this.spellName = "";
+  this.spellDamage = 0;
 
   this.toString = function() {
-    return this.name + " of " + this.type + " for " + this.damage + " damage!";
+    return this.spellName + " have been used for " + this.spellDamage + " damage!";
   }
 };
 
-/*
-  An elemental sphere that can be cast by a magical class
- */
-Gauntlet.SpellBook.Sphere = function() {
-  this.name = "sphere";
-  this.damage = Math.floor(Math.random() * 10 + 10);
-
-  var random = Math.round(Math.random() * (this.damageTypes.length - 1));
-  this.type = this.damageTypes[random];
+Gauntlet.SpellBook.Emails = function() {
+  this.spellName = "Deleted emails";
+  this.spellDamage = Math.floor(Math.random() * 10 + 5);
 };
-Gauntlet.SpellBook.Sphere.prototype = new Gauntlet.SpellBook.Spell();
+Gauntlet.SpellBook.Emails.prototype = new Gauntlet.SpellBook.Spell();
+
+
+Gauntlet.SpellBook.angryCats = function() {
+  this.spellName = "Angry cats";
+  this.spellDamage = Math.floor(Math.random() * 10 + 5);
+
+};
+Gauntlet.SpellBook.Emails.prototype = new Gauntlet.SpellBook.Spell();
+
+})(Gauntlet || {});
